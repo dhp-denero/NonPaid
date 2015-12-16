78556821b808d63cc502fa5da15ae8ed8e70c08b
TAGSBM <agardiner@tagsbm.com>
2015-12-16 13:03:46 +1100

    Custom layout for invoices
    Custom layout for invoices

---
+++ vuente_custom_invoice/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Custom Invoices(Vuente)",
+    'version': "1.1",
+    'author': "Vuenete",
+    'category': "Tools",
+    'summary': "Modifies invoice layout",
+    'data': [
+        'ir.ui.view.csv',
+        'res_company.xml',
+    ],
+    'demo': [],
+    'images':[
+    'static/description/1.jpg',
+    ],
+    'depends': ['web', 'account','report'],
+    'installable': True,
+}
---
+++ vuente_custom_invoice/doc/changelog.rst
@@ -0,0 +1,5 @@
+<h3>v1.1</h3>
+ - Added ACN and ABN numbers to res.company
+<h3>v1.0</h3><br>
+ - shift customer info to left and company info to right on same line<br/>
+ - Place ACN and ABN number info above company info
