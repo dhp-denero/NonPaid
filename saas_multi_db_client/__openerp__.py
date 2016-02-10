db9e5290a6288d15c1e4f531e1048345e3f1eab9
Vuente <anthony@anthonygardiner.com>
2016-02-10 20:58:26 +1100

    Create special saas user that becomes the new user upon system creation
    Create special saas user that becomes the new user upon system creation

---
+++ saas_multi_db_client/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "SAAS Multi Database Client",
+    'version': "1.0",
+    'author': "Vuente",
+    'category': "Tools",
+    'summary':'Adds special saas user which is designed to be used with saas_multi_db',
+    'license':'LGPL-3',
+    'data': [
+        'data/res.users.csv',
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': [],
+    'installable': True,
+}
