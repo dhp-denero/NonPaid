00cdc61869ab2a00a928488bff9246054479f445
Vuente <anthony@anthonygardiner.com>
2016-02-16 17:09:07 +1100

    opens in new tab
    opens in new tab

--- vuente_custom_crm_v9/__openerp__.py
+++ vuente_custom_crm_v9/__openerp__.py
@@ -4,7 +4,7 @@
     'author': "Vuente",
 	'website': 'http://www.vuente.com',
     'category': "Tools",
-    'depends': ['crm'],
+    'depends': ['crm','vuente_how_to_use_odoo_support_menu'],
     'data': [
         'views/res_partner.xml',
 	'views/crm_lead.xml',
