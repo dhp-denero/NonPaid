bd8ef2774069c88602351d6308bf3f7868fe6569
Vuente <anthony@anthonygardiner.com>
2016-03-02 13:45:55 +1100

    Dollar symbol is in front for aus
    Dollar symbol is in front for aus

---
+++ vuente_custom_website_event/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Custom Website Events",
+    'version': "1.0",
+    'author': "Vuente",
+	'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['website_event_sale','vuente_how_to_use_odoo_support_menu'],
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
