9778e7b583d8995e2a444db362629af43db2541f
Vuente <anthony@anthonygardiner.com>
2016-03-21 13:13:32 +1100

    remove menu and footer on particular pages and setup buy now button
    remove menu and footer on particular pages and setup buy now button

---
+++ website_sales_page/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website Sales Page",
+    'version': "1.1",
+    'author': "Vuente",
+    'category': "Tools",
+    'summary': "Let's you create pages without menu's and with buy now buttons",
+    'description': "Let's you create pages without menu's and with buy now buttons",
+    'license':'LGPL-3',
+    'data': [
+        'views/snippets.xml',
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': ['website','product', 'website_sale'],
+    'installable': True,
+}
