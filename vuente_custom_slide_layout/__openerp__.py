b36e2dbe09f53855884960033b0b1fa36b3d6d9a
Vuente <anthony@anthonygardiner.com>
2016-05-01 11:46:38 +1000

    custom slide layout
    custom slide layout

---
+++ vuente_custom_slide_layout/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Custom Slide Layout",
+    'version': "1.0",
+    'author': "Vuente",
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['website_slides','vuente_how_to_use_odoo_support_menu'],
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
