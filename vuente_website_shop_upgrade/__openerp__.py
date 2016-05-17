3dcf44360268c2daf767c43b33ec2815d0f9be21
Vuente <anthony@anthonygardiner.com>
2016-05-17 13:19:23 +1000

    Upgrades Vuente base system giving it website shop
    Upgrades Vuente base system giving it website shop

---
+++ vuente_website_shop_upgrade/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Website Shop Upgrade",
+    'version': "1.0",
+    'author': "Vuente",
+    'summary': 'Upgrades Vuente base system giving it website shop capactities',
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['website_sale'],
+    'data': [
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
