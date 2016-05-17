4872cf82b7776e7e2d149658bca0b52eecf709d7
Vuente <anthony@anthonygardiner.com>
2016-05-17 12:51:37 +1000

    Installs Vuente online event related changes
    Installs Vuente online event related changes

---
+++ vuente_online_events_upgrade/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Online Events Upgrade",
+    'version': "1.0",
+    'author': "Vuente",
+    'summary': 'Upgrades Vuente base system giving it online events capactities',
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['website_event'],
+    'data': [
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
