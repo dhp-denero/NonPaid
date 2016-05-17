fa772817ee619b241899a0505eae5fba6a4f7cb1
Vuente <anthony@anthonygardiner.com>
2016-05-17 12:29:59 +1000

    Inventory Upgrade Module
    Inventory Upgrade Module

---
+++ vuente_inventory_management_upgrade/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Inventory Management Upgrade",
+    'version': "1.0",
+    'author': "Vuente",
+    'summary': 'Upgrades Vuente base system giving it inventory management capactities',
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['stock'],
+    'data': [
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
