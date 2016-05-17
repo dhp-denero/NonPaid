1e619885132a9aa61b6246caca3a70a07aaeab8f
Vuente <anthony@anthonygardiner.com>
2016-05-17 13:27:04 +1000

    automation holding module
    automation holding module

---
+++ vuente_automation/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Vuente Automation",
+    'version': "1.0",
+    'author': "Vuente",
+    'summary': 'Holds all automation modules',
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['vuente_automated_task_tunnels'],
+    'data': [
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
