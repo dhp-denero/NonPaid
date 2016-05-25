74884c0b9aefce5f0e2b624922fbde1b22a87209
Vuente <anthony@anthonygardiner.com>
2016-05-25 14:36:21 +1000

    Add Custom back / fore color picker
    Add Custom back / fore color picker

--- vuente_web_kit/static/src/js/summerfun.js
+++ vuente_web_kit/static/src/js/summerfun.js
@@ -3,8 +3,53 @@ odoo.define('vuente_web_kit.summernote_fontfamily', function (require) {
 
 var core = require('web.core');
 var summerEditor = require('web_editor.rte');
-//require('summernote/summernote'); // wait that summernote is loaded
+var options = require('web_editor.snippets.options');
+require('summernote/summernote'); // wait that summernote is loaded
 var _t = core._t;
+var qweb = core.qweb;
+var ajax = require('web.ajax');
+
+ajax.loadXML('/vuente_web_kit/static/src/xml/website_color_picker_modal5.xml', qweb);
+
+options.Class.include({
+	    start: function () {
+			if ( $("#custombgcolour").length == 0) {
+			    $("div[title='Transparent']").eq(0).before("<div class='note-color-reset'><a id='custombgcolour'>Custom Back Colour</a></div>");
+			    $("div[title='Reset']").eq(0).before("<div class='note-color-reset'><a id='customfrcolour'>Custom Fore Colour</a></div>");
+			    $("#custombgcolour").click(function() {
+     		        var self = this;
+		            this.template = 'website_color_picker.color_background_picker_modal';
+		            self.$modal = $( qweb.render(this.template, {}) );
+		            $('body').append(self.$modal);
+                    $('#oe_pick_background_modal').modal('show');
+
+                    self.$modal.find("#submit_background_color").on('click', function () {
+						var backColor = self.$modal.find("#colorbgpick").val();
+						$.summernote.pluginEvents.applyFont(null, null, null, null, backColor, null);
+
+                        //self.$target.css("background-color",self.$modal.find("#colorpick").val() );
+                        self.$modal.modal('hide');
+                    });
+
+			    });
+			    $("#customfrcolour").click(function() {
+     		        var self = this;
+		            this.template = 'website_color_picker.color_picker_modal';
+		            self.$modal = $( qweb.render(this.template, {}) );
+		            $('body').append(self.$modal);
+                    $('#oe_pick_color_modal').modal('show');
+
+                    self.$modal.find("#submit_color").on('click', function () {
+						var foreColor = self.$modal.find("#colorfrpick").val();
+						$.summernote.pluginEvents.applyFont(null, null, null, foreColor, null, null);
+
+                        self.$modal.modal('hide');
+                    });
+
+			    });
+		    }
+    },
+});
 
 summerEditor.Class.include({
     config: function ($editable) {
