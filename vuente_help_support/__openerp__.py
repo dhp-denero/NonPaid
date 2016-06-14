8361a44a09640c290f5b1ce079c5da37547ec162
Vuente <anthony@anthonygardiner.com>
2016-06-14 22:01:36 +1000

    help documentation from within your system
    help documentation from within your system

---
+++ vuente_help_support/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Vuente Help and Support",
+    'version': "1.0",
+    'author': "Vuente",
+    'website': 'http://www.vuente.com',
+    'category': "Tools",
+    'depends': ['web'],
+    'data': [
+        'views/menus.xml',
+        'data/web_iframe_pages.page.csv',
+    ],
+    'demo': "www.vuente.com",
+    'installable': True,
+}
