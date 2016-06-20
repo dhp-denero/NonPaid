c6f17ab9e2e2b4e0bbbc21d4f1543cb0917a98fd
Vuente <anthony@anthonygardiner.com>
2016-06-20 10:44:12 +1000

    make it easier to install
    make it easier to install

--- vuente_help_support/__openerp__.py
+++ vuente_help_support/__openerp__.py
@@ -1,13 +1,18 @@
 {
     'name': "Vuente Help and Support",
-    'version': "1.0",
+    'version': "1.1",
     'author': "Vuente",
     'website': 'http://www.vuente.com',
     'category': "Tools",
     'depends': [],
     'data': [
         'views/menus.xml',
+        'views/web_iframe_views.xml',
+        'views/web_iframe_pages_views.xml',
         'data/web_iframe_pages.page.csv',
+    ],
+    'qweb': [
+        'static/src/xml/web_iframe.xml',
     ],
     'demo': "www.vuente.com",
     'installable': True,
---
+++ vuente_help_support/static/src/js/vuente_web_iframe.js
@@ -0,0 +1,40 @@
+openerp.vuente_help_support = function (session) {
+
+alert("test");
+
+    var _t = session.web._t,
+       _lt = session.web._lt;
+
+    var vuente_help_support = session.vuente_help_support;
+
+    session.web.client_actions.add('vuente_help_support.iframe', 'session.vuente_help_support.iframe');
+    vuente_help_support.iframe = session.web.Widget.extend({
+        template: 'vuente_help_support.iframe',
+
+        /**
+         * @param {Object} parent parent
+         * @param {Object} [options]
+         * @param {Array} [options.domain] domain on the Wall
+         * @param {Object} [options.context] context, is an object. It should
+         *      contain default_model, default_res_id, to give it to the threads.
+         */
+        init: function (parent, action) {
+            this._super(parent, action);
+            this.action = _.clone(action);
+            this.action.params = _.extend({
+                'link': 'https://yelizariev.github.io/',
+            }, this.action.params);
+        },
+        start: function() {
+            this.$el.find('iframe').css({height: '100%', width: '100%', border: 0});
+            this.$el.find('iframe').attr({src: this.action.params.link});
+            this.$el.find('iframe').on("load", this.bind_events.bind(this));
+            return this._super();
+        },
+        bind_events: function(){
+            //this.$el.find('iframe').contents().click(this.iframe_clicked.bind(this));
+        },
+        iframe_clicked: function(e){
+        }
+    })
+}
