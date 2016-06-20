44f51993a1571b5f836cf5d520b7353b85f83e8e
Vuente <anthony@anthonygardiner.com>
2016-06-20 11:28:55 +1000

    debrands notification template
    debrands notification template

---
+++ vuente_auto_mate/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Vuente Auto-Mate",
+    'version': "1.0",
+    'author': "Vuenete",
+    'category': "Tools",
+    'summary': "Everything related to automation",
+    'data': [
+       'views/email.xml',
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': ['crm'],
+    'installable': True,
+}
---
+++ vuente_auto_mate/doc/changelog.rst
@@ -0,0 +1,3 @@
+v1.0
+====
+* Replaces content of email template with ${object.body|safe}
