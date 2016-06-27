5e0d48acd23b1b9cdc53ca95002b1a7b1cd56433
Vuente <anthony@anthonygardiner.com>
2016-06-27 15:32:42 +1000

    Revision Number updated

--- vuente_help_support/__openerp__.py
+++ vuente_help_support/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Vuente Help and Support",
-    'version': "1.1.2",
+    'version': "1.1.3",
     'author': "Vuente",
     'website': 'http://www.vuente.com',
     'category': "Tools",
