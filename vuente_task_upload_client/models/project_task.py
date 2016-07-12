1e63d020b6dd36419d536a77b7a1cc5a8f505ace
Vuente <anthony@anthonygardiner.com>
2016-07-12 17:19:31 +1000

    encoding issue
    encoding issue

--- vuente_task_upload_client/models/project_task.py
+++ vuente_task_upload_client/models/project_task.py
@@ -6,6 +6,8 @@ from openerp.osv import osv
 
 import requests
 import werkzeug
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProjectTaskUpload(models.Model):
 
@@ -15,7 +17,11 @@ class ProjectTaskUpload(models.Model):
     def upload_task_vuente(self):
         server_url = "http://www.vuente.com"
         #server_url = "http://192.168.56.109:8069"
+        _logger.error( str(self.id) )
+        _logger.error( self.name.encode("utf-8") )
+        _logger.error( self.description.encode("utf-8") )
+        _logger.error( request.httprequest.host_url.encode("utf-8") )
         
-        return_string = requests.get(server_url + "/upload/task?task_id=" + str(self.id) + "&task_name=" + self.name.encode("utf-8") + "&task_description=" + self.description.encode("utf-8") + "&task_host=" + request.httprequest.host_url)
+        return_string = requests.get(server_url + "/upload/task?task_id=" + str(self.id) + "&task_name=" + self.name.encode("utf-8") + "&task_description=" + self.description.encode("utf-8") + "&task_host=" + request.httprequest.host_url.encode("utf-8") )
 
         raise osv.except_osv(("Warning"), ("Your task has been uploaded and will be reviewed soon"))  
