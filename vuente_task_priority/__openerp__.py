f74119fbd167db0a43a13f85b59c71ef850bf2e3
Vuente <anthony@anthonygardiner.com>
2016-07-25 10:44:09 +1000

    just adds one field, what was the point of this in v8?!?
    just adds one field, what was the point of this in v8?!?

---
+++ vuente_task_priority/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Vuente Task Priority",
+    'version': "1.0",
+    'author': 'Vuente',
+    'category': "Tools",
+    'data': [
+        'views/task_view.xml',
+    ],
+    'demo': [],
+    'depends': ['project'],
+    'installable': True,
+}
---
+++ vuente_task_priority/models/project_task.py
@@ -0,0 +1,12 @@
+from openerp import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+class ProjectTask(models.Model):
+
+    _inherit = "project.task"
+    _order = "create_date desc"
+    
+    task_url = fields.Char(string='Task URL Link')
+    pri = fields.Selection((('1','A1'), ('2','A2'), ('3','B1'),('4','B2'), ('5','C1'),('6','C2'), ('7','D1'),('8','D2')),string="Priority")
+
