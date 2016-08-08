425f9d065df7f36f09aba612814b689a7cb00123
Vuente <anthony@anthonygardiner.com>
2016-08-08 12:17:25 +1000

    Trying to simplify the import from Google contacts
    Trying to simplify the import from Google contacts

---
+++ vuente_import_multi/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Vuente Import Multi Select",
+    'version': "1.0",
+    'author': "Vuenete",
+    'category': "Tools",
+    'summary': "Changes import button to a multi select",
+    'data': [],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': [],
+    'qweb': ['static/src/xml/import.xml'],
+    'installable': True,
+}
