57ef112751d5b434f9a47f9178c57d047312d789
Vuente <anthony@anthonygardiner.com>
2017-01-30 14:08:32 +1100

    update the tutorials in all systems
    update the tutorials in all systems

---
+++ vuente_iframe_help_csv/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Vuente Iframe Help - CSV",
+    'version': '1.0',
+    'author': 'Vuente',
+    'category': 'Tools',
+    'depends': ['vuente_iframe_help'],
+    'data': [
+        'views/web_iframe_views.xml',
+        'data/vuente.iframe.page.csv',
+     ],
+    'installable': True
+}
