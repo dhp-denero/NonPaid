594d54b7143d72d253e5288e7cf0e648c9889004
Vuente <anthony@anthonygardiner.com>
2017-05-23 08:42:58 +1000

    fetchmail no read
    fetchmail no read

---
+++ vuente_mail_no_read/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Vuente Mail no Read",
+    'version': "1.0",
+    'author': "Vuente",
+    'category': "Tools",
+    'summary': "Mail is not marked as read",
+    'data': [
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': ['fetchmail'],
+    'installable': True,
+}
