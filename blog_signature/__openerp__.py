cda7257523d5f58e9bfd7115fe7a2bd03aefcbe7
Vuente Business Solutions <support@vuente.com>
2017-08-07 09:45:39 +1000

    Stage 1 Module Creation
    Anthony created this core module structure which adds a website snippet
    for a one time setup drag n drop blog signature

---
+++ blog_signature/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website Personilized Blog Signature Snippet",
+    'version': "1.1",
+    'author': "Vuente",
+    'category': "Tools",
+    'summary': "Create your personilized blog signature",
+    'description': "Create your personilized blog signature",
+    'license':'LGPL-3',
+    'data': [
+        'views/snippets.xml',
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': ['website', 'marketing_campaign'],
+    'installable': True,
+}
